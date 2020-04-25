bda50dce4bb515364544de24d2c0da17dd6a9cf8
SLJ <shenlujia@gmail.com>
2020-04-25 21:27:35 +0800

    thing go

---
+++ ThingGo/ThingGo/AppDelegate.h
@@ -0,0 +1,13 @@
+//
+//  AppDelegate.h
+//  ThingGo
+//
+//  Created by SLJ on 2019/12/18.
+//  Copyright © 2019 KLA. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@end
---
+++ ThingGo/ThingGo/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  ThingGo
+//
+//  Created by SLJ on 2019/12/18.
+//  Copyright © 2019 KLA. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
