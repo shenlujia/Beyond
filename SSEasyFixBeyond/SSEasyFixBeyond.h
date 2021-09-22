24ef28c868258028f05b89b5dfd892f461addea6
shenlujia <shenlujia@bytedance.com>
2021-09-22 17:55:07 +0800

    n

---
+++ SSEasyFixBeyond/SSEasyFixBeyond.h
@@ -0,0 +1,9 @@
+//
+//  SSEasyFixBeyond.h
+//  Beyond
+//
+//  Created by ZZZ on 2021/9/22.
+//  Copyright © 2021 SLJ. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
---
+++ SSEasyFixBeyond/SSEasyFixBeyond.m
@@ -0,0 +1,32 @@
+//
+//  SSEasyFixBeyond.m
+//  Beyond
+//
+//  Created by ZZZ on 2021/9/22.
+//  Copyright © 2021 SLJ. All rights reserved.
+//
+
+#import "SSEasyFixBeyond.h"
+#import <fishhook/fishhook.h>
+
+bool isTTVideoEngineLogEnabled = NO;
+
+void hmd_disable_cpp_exception_backtrace(void)
+{
+    
+}
+
+void alog_close(void)
+{
+    
+}
+
+void open_bdfishhook(void)
+{
+    
+}
+
+int bd_rebind_symbols(struct rebinding array[], size_t n)
+{
+    return rebind_symbols(array, n);
+}
