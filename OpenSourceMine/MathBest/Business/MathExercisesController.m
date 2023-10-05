406785aae6f49d6d6484f577439ee6a1c5625c6f
shenlujia <shenlujia@bytedance.com>
2023-10-05 09:19:41 +0800

    m

--- OpenSourceMine/MathBest/Business/MathExercisesController.m
+++ OpenSourceMine/MathBest/Business/MathExercisesController.m
@@ -177,6 +177,7 @@
         s.autoresizingMask = UIViewAutoresizingFlexibleTopMargin | UIViewAutoresizingFlexibleBottomMargin | UIViewAutoresizingFlexibleLeftMargin;
         s.on = [configuration boolForFeature:feature];
         s.tag = feature;
+        s.onTintColor = [UIColor.greenColor colorWithAlphaComponent:0.25];
         [s addTarget:self action:@selector(switchAction:) forControlEvents:UIControlEventValueChanged];
         blockSwitch = s;
     } tap:^(UIButton *button, NSDictionary *userInfo) {
