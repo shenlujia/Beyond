4b9cbb08fda7bda752d7ff348f02ab21df1944b2
ZZZ <>
2026-04-04 09:54:27 +0800

    m

---
+++ CrackDemo/NameCenter/FindMe.h
@@ -0,0 +1,14 @@
+//
+//  FindMe.h
+//  NameCenter
+//
+//  Created by ZZZ on 2022/10/3.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FindMe : NSObject
+
++ (NSArray *)find:(NSString *)string from:(NSArray *)from;
+
+@end
